2a95fb2fa23bd7a3e4bef791049dab8c92638be7
xiangdeyong <xiangdeyong@outlook.com>
2020-04-08 00:24:27 +0800

    variable

--- src/main/java/com/xiangdy/study/base/Variable.java
+++ src/main/java/com/xiangdy/study/base/Variable.java
@@ -10,11 +10,12 @@ public class Variable {
     /**
      * 变量存储区域及大小
      *
-     * 变量自动转型内容
+     * 变量运算符 【变量自动转型】
      *
      */
     public void testVariable(){
-
+        // 函数栈内定义一个num的变量 指向栈中的10的值
+        int num = 10;
     }
 
 
