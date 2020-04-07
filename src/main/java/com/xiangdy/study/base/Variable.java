85f9827feddbbbb5956a1e875c240087ffc802f3
xiangdeyong <xiangdeyong@outlook.com>
2020-04-08 00:17:57 +0800

    variable

---
+++ src/main/java/com/xiangdy/study/base/Variable.java
@@ -0,0 +1,21 @@
+package com.xiangdy.study.base;
+
+/**
+ * @author xiangDY
+ * @Description: java基础变量
+* @date 2020/4/8　0:15
+ */
+public class Variable {
+
+    /**
+     * 变量存储区域及大小
+     *
+     * 变量自动转型内容
+     *
+     */
+    public void testVariable(){
+
+    }
+
+
+}
